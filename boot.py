8594ea1922a2235c84f3c613e98010bba49e7c26
Timo Noko <timonoko@users.noreply.github.com>
2020-09-14 17:31:54 +0300

    dfs

--- boot.py
+++ boot.py
@@ -6,7 +6,7 @@ def do_connect():
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect('Jorpakko', 'Juhannusyona')
+        sta_if.connect('sid', 'pass')
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
